01b8fc061f935a660b41f41bfc2ca6f456252d7a
INF1995 <inf1995@localhost.localdomain>
2018-03-13 11:21:00 -0400

    lire/ecrire mem

---
+++ repo-6165/tp/tp9/library/mem.cpp
@@ -0,0 +1,30 @@
+#include <mem.h>
+
+uint8_t* lecture() {
+
+	Memoire24CXXX memoire = Memoire24CXXX(); //initialisation de la memoire
+	memoire.init();
+	
+		
+	uint8_t octet1 = 0x0;
+	uint8_t octet2 = 0x0;	
+	uint16_t longeur = 0x0;
+	memoire.lecture(0x0, &octet1);
+	memoire.lecture(0x1, &octet2);
+	
+	longeur = (octet1 << 8) + octet2;
+	unsigned char donneesSortie[longeur+1];
+	memoire.lecture(0x2, donneesSortie,longeur);	
+	
+	return donneesSortie;
+}
+
+void ecrireDonnees(unsigned char * donneesEntrees,uint8_t longueur) {
+
+	
+	Memoire24CXXX memoire = Memoire24CXXX();
+	memoire.init();
+	
+	memoire.ecriture(0x00,donneesEntrees,longueur);
+	
+}
---
+++ repo-6165/tp/tp9/library/mem.h
@@ -0,0 +1,7 @@
+#include "memoire_24.h"
+//~ #include <avr/io.h> 
+//~ #include <util/delay_basic.h>
+//~ #include <util/delay.h>
+
+uint8_t* lecture();
+void ecrireDonnees(unsigned char * donneesEntrees,uint8_t longueur);
