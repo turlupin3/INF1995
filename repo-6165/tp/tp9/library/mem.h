d5028071976b4e1681145505877058088ba53479
INF1995 <inf1995@localhost.localdomain>
2018-03-20 09:24:56 -0400

    fuck outta here mem.h

--- repo-6165/tp/tp9/library/mem.h
+++
@@ -1,7 +0,0 @@
-#include "memoire_24.h"
-//~ #include <avr/io.h> 
-//~ #include <util/delay_basic.h>
-//~ #include <util/delay.h>
-
-uint8_t* lecture();
-void ecrireDonnees(unsigned char * donneesEntrees,uint8_t longueur);
